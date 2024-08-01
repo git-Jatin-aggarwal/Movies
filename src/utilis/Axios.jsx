ab69e157b0447dd372091a9605fdc10f826bcc60
Jatin Aggarwal <153506934+git-Jatin-aggarwal@users.noreply.github.com>
2024-08-01 17:27:13 +0530

    Update Axios.jsx

--- src/utilis/Axios.jsx
+++ src/utilis/Axios.jsx
@@ -5,7 +5,7 @@ const instance = axios.create({
     baseURL:"https://api.themoviedb.org/3" ,
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTJlZDk4N2Q0MTFlYTliM2U3MmU5ZGQzNjQ2MTBjZiIsIm5iZiI6MTcyMjQ5MjAzNS40MjQzMTYsInN1YiI6IjY2YWIyMGU4OWM1MWE0ODdkYmExMzBiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d4r-2ikxpqzuaZ9tuDx-fUXTUcXlvlSPX70to6TSbwo'
+        Authorization: 'PX70to6TSbwo'
       }
 })
 
